0c9508dcd5500a0528bfeb0de075013d270c617b
Andres Isaza <andres.isaza1506@gmail.com>
2019-08-07 10:13:33 -0500

    MercadolibreAPI

---
+++ src/app/components/search/search.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { ProductsService } from '../../services/products.service';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html'
+})
+export class SearchComponent {
+  products = [];
+  constructor(private productsService: ProductsService) { }
+
+  search(query: string) {
+    this.productsService.getProducts(query).subscribe(products => {
+      this.products = products;
+      console.log(this.products);
+
+    });
+  }
+
+}
---
+++ src/app/services/products.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+  BASE_URL = 'https:api.mercadolibre.com';
+
+  constructor(private http: HttpClient) { }
+
+  getProducts(query: string) {
+    return this.http.get(`${this.BASE_URL}/sites/MCO/search?q=${query}`)
+      .pipe(map((res: Object) => {
+        let results = res['results'];
+        let products = [];
+        Object.keys(results).forEach(key => {
+          const product = results[key];
+          products.push(product);
+        });
+        return products;
+      }));
+  }
+
+  getProduct(id: string) {
+    return this.http.get(`${this.BASE_URL}/items/${id}`);
+  }
+
+  getSeller(sellerID: Number) {
+    return this.http.get(`${this.BASE_URL}/users/${sellerID}`);
+  }
+}
