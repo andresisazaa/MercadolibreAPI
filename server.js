5c2b76542d1f78fa44224a00748e16767387a8de
Andres Isaza <andres.isaza1506@gmail.com>
2019-08-13 15:17:50 -0500

    Fixed project route

--- server.js
+++ server.js
@@ -4,14 +4,12 @@ const path = require('path');
 
 const app = express();
 
-const cors = require("cors");
-app.use(cors());
-
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/MERCADOLIBREAPI'));
+app.use(express.static(__dirname + '/dist/mercadolibre-api'));
 
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/MERCADOLIBREAPI/index.html'));
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/mercadolibre-api/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
